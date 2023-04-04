8990505099c96241ce6783b22cffd36c612eb4cb
livivarga <76699785+livivarga@users.noreply.github.com>
2023-04-04 18:19:24 +0200

    practice backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
 const app = express()
 const port = 9000
 
@@ -16,6 +17,42 @@ app.get('/script.js', (req, res) => {
 })
 */
 app.use('/public', express.static(`${__dirname}/../frontend/public`))
+
+app.get('/data', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/data/data.json`))
+})
+
+app.get('/data/:id', (req, res) => {
+	//console.log(req.params)
+	try {
+		const searchId = parseInt(req.params.id)
+		//console.log(searchId)
+		if(isNaN(searchId)) {
+			res.status(418).send('Not a number')
+		} else {
+			fs.readFile(`data/data.json`, (err, data) => {
+				let result = null
+				const fileData = JSON.parse(data)
+				//console.log(fileData)
+				for (let index = 0; index < fileData.length; index++) {
+					const element = fileData[index];
+					if (element.id === searchId) {
+						console.log(element)
+						result = element
+					}
+				}
+				if (result === null) {
+					res.status(404).send('Nincs ilyen user')
+				} else {
+					res.send(result)
+				}
+			})
+		}
+	} catch(error) {
+		console.log(error)
+		res.send('It is an error!')
+	}
+})
 
 app.listen(port, () => {
   console.log(`http://127.0.0.1:${port}`)
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,1 +1,18 @@
 console.log('loaded')
+
+fetch(`http://127.0.0.1:9000/data`)
+  .then(response => {
+    console.log(response)
+    if (response.status === 200) {
+      console.log('OK')
+    }
+    return response.json()
+  })
+
+  .then(responseJson => {
+    const data = responseJson
+    data.forEach(element => {
+      console.log(element.name)
+      document.querySelector('#root').insertAdjacentHTML('beforeend', `<h2>${element.name}</h2>`)
+    });
+  })
