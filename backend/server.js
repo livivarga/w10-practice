73fb433d23cc1377295b8c57c7c2458841584c36
livivarga <76699785+livivarga@users.noreply.github.com>
2023-04-05 15:21:26 +0200

    practice uploading data and files

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,56 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
 const app = express()
 const port = 9000
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../frontend/index.html`))
+	res.sendFile(path.join(`${__dirname}/../frontend/index.html`))
 })
 
 app.use('/public', express.static(`${__dirname}/../frontend/public`))
 
+app.get('/data', (req, res) => {
+	res.sendFile(path.join(`${__dirname}/data/data.json`))
+})
+
+app.get('/data/:id', (req, res) => {
+	//console.log(req.params)
+	try {
+		const searchId = parseInt(req.params.id)
+		//console.log(searchId)
+		if (isNaN(searchId)) {
+			res.status(418).send('Not a number')
+		} else {
+			fs.readFile(`${__dirname}/data/data.json`, (err, data) => {
+				if (err) {
+					console.log(err)
+					res.send(err)
+				} else {
+					let result = null
+					const fileData = JSON.parse(data)
+					//console.log(fileData)
+					for (let index = 0; index < fileData.length; index++) {
+						const element = fileData[index];
+						if (element.id === searchId) {
+							console.log(element)
+							result = element
+						}
+					}
+					if (result === null) {
+						res.status(404).send('Nincs ilyen user')
+					} else {
+						res.send(result)
+					}
+				}
+			})
+		}
+	} catch (error) {
+		console.log(error)
+		res.send('It is an error!')
+	}
+})
+
 app.listen(port, () => {
-  console.log(`http://127.0.0.1:${port}`)
+	console.log(`http://127.0.0.1:${port}`)
 })
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,1 +1,38 @@
 console.log('loaded')
+
+const formComponent = () => `
+  <form>
+    <input type="text" name="name" placeholder="image name">
+    <input type="file" name="file">
+    <button>SEND</button>
+  </form>
+`
+const rootElement = document.querySelector('#root')
+
+rootElement.insertAdjacentHTML('beforeend', formComponent())
+
+const formElement = document.querySelector('form')
+
+formElement.addEventListener('submit', (event) => {
+  event.preventDefault()
+  console.log('Submit')
+
+  const formData = new FormData()
+  formData.append('name', document.querySelector("input[type='text']").value)
+  formData.append('image', document.querySelector("input[type='file']").files[0])
+  fetch('/upload', {
+    method: 'POST',
+    body: formData
+  })
+  .then(res => {
+    if (res.status === 200) {
+      console.log('success')
+      return res.json()
+    } else {
+      console.log('ERROR!!!')
+    }
+  })
+  .then(resJson => console.log(resJson))
+  .catch(error => console.log(error))
+})
+
